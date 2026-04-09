f2a8fdfa912b16ac4101d520e4417d77a4196b38
MikeL538 <mikel538.work@gmail.com>
2026-04-09 20:10:18 +0200

    fix: vite config for dist build

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: new URL("./index.html", import.meta.url).pathname,
+        projects: new URL("./projects.html", import.meta.url).pathname,
+      },
+    },
+  },
 });
