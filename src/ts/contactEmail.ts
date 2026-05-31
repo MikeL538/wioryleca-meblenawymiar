2182de933f7993bc366b6f76b24a857348f6875e
MikeL538 <mikel538.work@gmail.com>
2026-05-31 21:21:26 +0200

    feat: working contact email

--- src/ts/contactEmail.ts
+++ src/ts/contactEmail.ts
@@ -1,5 +1,5 @@
 const sendButton = document.querySelector<HTMLButtonElement>("#contactButton");
-// const name = document.querySelector<HTMLInputElement>("#contactName");
+const name = document.querySelector<HTMLInputElement>("#contactName");
 const contact = document.querySelector<HTMLInputElement>("#contactContact");
 const message = document.querySelector<HTMLTextAreaElement>("#contactMessage");
 const preferable =
@@ -11,12 +11,13 @@ sendButton?.addEventListener("click", async (event) => {
 });
 
 async function sendEmail() {
-  const response = await fetch("http://localhost:3000/send-email", {
+  const response = await fetch("https://project-6j4p0.vercel.app/send-email", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
+      name: name?.value,
       contact: contact?.value,
       message: message?.value,
       preferable: preferable?.value,
